fa274dd8ad8befdcd54b1cbf8024019e1c4578e6
l.j.d.beekman@student.utwente.nl <l.j.d.beekman@student.utwente.nl>
2020-12-05 11:06:45 +0100

    removal of useless function mySplit

--- Day1.hs
+++ Day1.hs
@@ -2,12 +2,6 @@ readInt x = read x::Int
 
 file_numbers = ((fmap readInt) . lines) <$> readFile("InputDay1.txt")
 
-mysplit' cur char [] = [cur]
-mysplit' cur char (x:xs) | char==x   = cur:mysplit' [] char xs
-                         | otherwise = mysplit' (cur ++ [x]) char xs
-
-mysplit char xs = mysplit' [] char xs
-
 find_combo' x [] = []
 find_combo' x (y:ys)  | x+y == 2020 = [x*y]
                       | otherwise = find_combo' x ys
