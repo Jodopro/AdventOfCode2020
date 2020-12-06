2c4cec3801f33b4affe0f1ffbec3b44defd995e1
l.j.d.beekman@student.utwente.nl <l.j.d.beekman@student.utwente.nl>
2020-12-06 12:02:17 +0100

    well, maybe i did one-liners for day 6

--- Day6.hs
+++ Day6.hs
@@ -1,12 +1,6 @@
 import Data.List.Split
 import Data.List
 
-file_answers = ((fmap $ splitOn "\n") . (splitOn "\n\n")) <$> readFile("InputDay6.txt")
+part1 = (sum . (fmap (\answers -> length $ nub $ foldr (++) [] answers)) . (fmap $ splitOn "\n") . (splitOn "\n\n")) <$> readFile("InputDay6.txt")
 
-getCount1 answers = length $ nub $ foldr (++) [] answers
-
-getCount2 (x:answers) = length $ filter (\x -> x) $ (foldr (&&) True) <$> (\y -> (elem y) <$> answers) <$> x
-
-part1 = (sum . (fmap $ getCount1)) <$> file_answers
-
-part2 = (sum . (fmap $ getCount2)) <$> file_answers
+part2 = (sum . (fmap (\(x:answers) -> length $ filter (\x -> x) $ (foldr (&&) True) <$> (\y -> (elem y) <$> answers) <$> x)) . (fmap $ splitOn "\n") . (splitOn "\n\n")) <$> readFile("InputDay6.txt")
